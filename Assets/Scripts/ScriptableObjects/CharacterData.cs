6256c896133d5d2001d26f11bee927a3abc352a9
Kevin_Ibanez <kevin.ibanezpro@gmail.com>
2020-04-07 19:19:11 +0200

    Scripts

---
+++ Assets/Scripts/ScriptableObjects/CharacterData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character",menuName = "Create a new character")]
+public class CharacterData : ScriptableObject
+{
+    public string CharacterName;
+    public float HP;
+    public float HPRegain;
+    public float Shield;
+    public float Damage;
+    public float SpellDamage;
+    public float CDR;
+    public float AtkSpeed;
+    public float ReloadSpeed;
+    public float MoveSpeed;
+    public int JumpNumber;
+}
