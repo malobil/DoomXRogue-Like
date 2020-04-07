81996bb4d72fecd3e593be2d288f4d838e076f38
Kevin_Ibanez <kevin.ibanezpro@gmail.com>
2020-04-07 22:29:37 +0200

    Input system

---
+++ Assets/Scripts/Characters.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Characters : IDamageable,IHealable
+{
+    [SerializeField] private CharacterData CharacterStats;
+
+    public virtual void Heal(float healTake)
+    {
+        
+    }
+
+    public virtual void TakeDamage(float damageTaken)
+    {
+       
+    }
+
+    public virtual void Shot()
+    {
+
+    }
+
+    public virtual void OnEnter()
+    {
+
+    }
+
+    public virtual void OnUpdate()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnEnter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnUpdate();
+    }
+}
